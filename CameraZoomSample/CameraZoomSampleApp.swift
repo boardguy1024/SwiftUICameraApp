c0daba5490f99385de11887e94ef942e00c31d85
paku <boardguy1024@gmail.com>
2024-04-11 15:36:54 +0900

    Initial Commit

---
+++ CameraZoomSample/CameraZoomSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraZoomSampleApp.swift
+//  CameraZoomSample
+//
+//  Created by paku on 2024/04/11.
+//
+
+import SwiftUI
+
+@main
+struct CameraZoomSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraZoomSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CameraZoomSample
+//
+//  Created by paku on 2024/04/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
